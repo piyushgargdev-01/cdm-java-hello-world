1437c163c8cc28e1aee2b84c191ff9ba8b85c162
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-09-21 00:36:24 +0530

    Changes

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
-// Write your code here...
+public class Main {
+    public static void main(String args[]) {
+
+        // <data_type> variable_name = <value>
+
+        int a = 10;
+        int age = 22;
+
+        System.out.println(age); // Outputs: 22
+
+        age = 30; // Re-assignment
+
+        System.out.println(age); // Outputs: 30;
+        System.out.println(a); // Outputs: 10;
+    }
+}
