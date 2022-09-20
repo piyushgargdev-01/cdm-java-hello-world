66abd93748723782226d8749c14a47320b84118f
Piyush Garg Dev <piyushgargdev@Piyushs-MacBook-Pro.local>
2022-09-21 00:42:13 +0530

    Added Code

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
-// Write your code here...
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Value of A: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter Value of B: ");
+        int b = scanner.nextInt();
+
+        System.out.println("Sum: " + (a + b));
+        System.out.println("Divide: " + (b - a));
+        System.out.println("Multiply: " + (a * b));
+        System.out.println("Divide: " + (a / b));
+    }
+}
